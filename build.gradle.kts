67c6c7e82868756edf3a6957f57fb2825ff9a546
Oliver Nybroe <oliver@worksome.com>
2021-10-01 08:41:55 +0200

    Add back Qodana show report env option

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ qodana {
     cachePath.set(projectDir.resolve(".qodana").canonicalPath)
     reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
     saveReport.set(true)
-    // showReport.set(System.getenv("QODANA_SHOW_REPORT").toBoolean())
+    showReport.set(System.getenv("QODANA_SHOW_REPORT").toBoolean())
 }
 
 tasks {
